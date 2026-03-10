8bd75d5f0a040c832622edc02331219d3bd388d1
Aayush Kumar <aayush2615@gmail.com>
2026-03-10 15:14:29 +0530

    1st program

---
+++ p1.py
@@ -0,0 +1,19 @@
+# Simple Reflex Agent for Vacuum Cleaner World
+
+# Function to simulate the vacuum cleaner agent
+def reflex_vacuum_agent(location, status):
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Right"
+    elif location == "B":
+        return "Left"
+
+
+location = "A"
+status = "Dirty"
+
+
+action = reflex_vacuum_agent(location, status)
+print(f"Location: {location}, Status: {status}, Action: {action}")
+
